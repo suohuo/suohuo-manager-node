c6b20206ea75ccdfc39994d55c6e158145dc1815
sunyimin <sunyimin@duiba.com.cn>
2020-04-28 18:26:37 +0800

    menu

---
+++ src/components/menu/const.ts
@@ -0,0 +1,21 @@
+const MENU_CONFIG = [{
+  title: '用户管理',
+  key: 'userManger',
+  children: [{
+    title: '个人中心',
+    key: 'userCenter'
+  }, {
+    title: '账号管理',
+    key: 'account'
+  }]
+}, {
+  title: '其他',
+  key: 'other',
+  children: [{
+    title: '权限中心',
+    key: 'permission'
+  }]
+}]
+
+
+export default MENU_CONFIG
--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -1,8 +1,13 @@
 import React, { useState } from 'react'
 import { Menu } from 'antd';
 import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
+import MENU_CONFIG from './const'
 
 const { SubMenu } = Menu;
+
+const MenuStyle: React.CSSProperties = {
+  width: 191
+}
 
 const MenuList: React.FC = () => {
     const rootSubmenuKeys = ['sub1', 'sub2', 'sub4']
@@ -24,49 +29,27 @@ const MenuList: React.FC = () => {
         mode="inline"
         openKeys={openKeys}
         onOpenChange={onOpenChange}
-        style={{ width: 191 }}
+        style={MenuStyle}
         >
-        <SubMenu
-          key="sub1"
-          title={
-            <span>
-              <MailOutlined />
-              <span>个人中心</span>
-            </span>
-          }
-        >
-          <Menu.Item key="1">Option 1</Menu.Item>
-          <Menu.Item key="2">Option 2</Menu.Item>
-          <Menu.Item key="3">Option 3</Menu.Item>
-          <Menu.Item key="4">Option 4</Menu.Item>
-        </SubMenu>
-        <SubMenu
-          key="sub2"
-          title={
-            <span>
-              <AppstoreOutlined />
-              <span>用户管理</span>
-            </span>
-          }
-        >
-          <Menu.Item key="5">Option 5</Menu.Item>
-          <Menu.Item key="6">Option 6</Menu.Item>
-        </SubMenu>
-        <SubMenu
-          key="sub4"
-          title={
-            <span>
-              <SettingOutlined />
-              <span>Navigation Three</span>
-            </span>
-          }
-        >
-          <Menu.Item key="9">Option 9</Menu.Item>
-          <Menu.Item key="10">Option 10</Menu.Item>
-          <Menu.Item key="11">Option 11</Menu.Item>
-          <Menu.Item key="12">Option 12</Menu.Item>
-        </SubMenu>
-      </Menu>
+         {
+           MENU_CONFIG?.map((item, index) => {
+              return (
+                <SubMenu
+                 key="sub1"
+                 title={<span><MailOutlined /><span>{item.title}</span></span>}
+                >
+                  {
+                    item.children?.map((item, index) => {
+                       return (
+                        <Menu.Item key={index}>{item.title}</Menu.Item>
+                       )
+                    })
+                  }
+                </SubMenu>
+              )
+           })
+         }
+       </Menu>
       </>
     )
 }
